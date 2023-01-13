1911ad667c88046fe902730c46f8ad03d4b7a7de
Gil Torovezky <gilitorovezky@gmail.com>
2023-01-13 12:51:54 -0800

    Adding
    Signed-off-by: Gil Torovezky <gilitorovezky@gmail.com>

--- config/constants.php
+++ config/constants.php
@@ -4,9 +4,9 @@ session_start();
 
 //Create Constants to save Database Credentials
 define('LOCALHOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'task_manager');
+define('DB_USERNAME', 'user001');
+define('DB_PASSWORD', '1234');
+define('DB_NAME', 'y9');
 
 define('SITEURL', 'http://localhost/task-manager/');
 
